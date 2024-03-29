069615a9c9d640fe8d5d99729e6e40c9f919280b
Jared Wray <me@jaredwray.com>
2024-03-29 16:00:40 -0700

    migrating to xo for linting

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       reporter: ['json', 'text'],
-      exclude: ['test', 'examples', '.eslintrc.js', 'vite.config.ts'],
+      exclude: ['test', 'examples', 'vite.config.ts'],
     },
   },
 });
