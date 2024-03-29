0a3f534dc192c72d43434410d42cb68c346e6bb7
Jared Wray <me@jaredwray.com>
2024-03-29 16:33:22 -0700

    updated memory.test.ts

--- test/utils.ts
+++ test/utils.ts
@@ -1,25 +1,27 @@
 import process from 'node:process';
 
-export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+// eslint-disable-next-line no-promise-executor-return
+export const sleep = async (ms: number) => new Promise(r => setTimeout(r, ms));
 
 export const disableExistingExceptionListeners = () => {
-  const uncaughtExceptionListeners = process.rawListeners(
-    'uncaughtException',
-  ) as NodeJS.UncaughtExceptionListener[];
-  const unhandledRejectionListeners = process.rawListeners(
-    'unhandledRejection',
-  ) as NodeJS.UnhandledRejectionListener[];
+	const uncaughtExceptionListeners = process.rawListeners(
+		'uncaughtException',
+	) as NodeJS.UncaughtExceptionListener[];
+	const unhandledRejectionListeners = process.rawListeners(
+		'unhandledRejection',
+	) as NodeJS.UnhandledRejectionListener[];
 
-  process.removeAllListeners('uncaughtException');
-  process.removeAllListeners('unhandledRejection');
+	process.removeAllListeners('uncaughtException');
+	process.removeAllListeners('unhandledRejection');
 
-  /* restore listeners */
-  return () => {
-    uncaughtExceptionListeners.forEach((listener) =>
-      process.addListener('uncaughtException', listener),
-    );
-    unhandledRejectionListeners.forEach((listener) =>
-      process.addListener('unhandledRejection', listener),
-    );
-  };
+	/* Restore listeners */
+	return () => {
+		for (const listener of uncaughtExceptionListeners) {
+			process.addListener('uncaughtException', listener);
+		}
+
+		for (const listener of unhandledRejectionListeners) {
+			process.addListener('unhandledRejection', listener);
+		}
+	};
 };
