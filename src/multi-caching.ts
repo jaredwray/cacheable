7da650658374e2c3b74d4780eb36517f330ca8d6
Jared Wray <me@jaredwray.com>
2024-03-29 16:02:16 -0700

    initial xo format

--- src/multi-caching.ts
+++ src/multi-caching.ts
@@ -1,85 +1,98 @@
-import { Cache, Milliseconds, WrapTTL } from './caching';
+import {type Cache, type Milliseconds, type WrapTTL} from './caching';
 
 export type MultiCache = Omit<Cache, 'store'> &
-  Pick<Cache['store'], 'mset' | 'mget' | 'mdel'>;
+Pick<Cache['store'], 'mset' | 'mget' | 'mdel'>;
 
 /**
  * Module that lets you specify a hierarchy of caches.
  */
 export function multiCaching<Caches extends Cache[]>(
-  caches: Caches,
+	caches: Caches,
 ): MultiCache {
-  const get = async <T>(key: string) => {
-    for (const cache of caches) {
-      try {
-        const val = await cache.get<T>(key);
-        if (val !== undefined) return val;
-      } catch (e) {}
-    }
-  };
-  const set = async <T>(
-    key: string,
-    data: T,
-    ttl?: Milliseconds | undefined,
-  ) => {
-    await Promise.all(caches.map((cache) => cache.set(key, data, ttl)));
-  };
+	const get = async <T>(key: string) => {
+		for (const cache of caches) {
+			try {
+				const value = await cache.get<T>(key);
+				if (value !== undefined) {
+					return value;
+				}
+			} catch {}
+		}
+	};
 
-  return {
-    get,
-    set,
-    del: async (key) => {
-      await Promise.all(caches.map((cache) => cache.del(key)));
-    },
-    async wrap<T>(
-      key: string,
-      fn: () => Promise<T>,
-      ttl?: WrapTTL<T>,
-      refreshThreshold?: Milliseconds
-    ): Promise<T> {
-      let value: T | undefined;
-      let i = 0;
-      for (; i < caches.length; i++) {
-        try {
-          value = await caches[i].get<T>(key);
-          if (value !== undefined) break;
-        } catch (e) {}
-      }
-      if (value === undefined) {
-        const result = await fn();
-        const cacheTTL = typeof ttl === 'function' ? ttl(result) : ttl;
-        await set<T>(key, result, cacheTTL);
-        return result;
-      } else {
-        const cacheTTL = typeof ttl === 'function' ? ttl(value) : ttl;
-        Promise.all(
-          caches.slice(0, i).map((cache) => cache.set(key, value, cacheTTL)),
-        ).then();
-        caches[i].wrap(key, fn, ttl, refreshThreshold).then(); // call wrap for store for internal refreshThreshold logic, see: src/caching.ts caching.wrap
-      }
-      return value;
-    },
-    reset: async () => {
-      await Promise.all(caches.map((x) => x.reset()));
-    },
-    mget: async (...keys: string[]) => {
-      const values = new Array(keys.length).fill(undefined);
-      for (const cache of caches) {
-        if (values.every((x) => x !== undefined)) break;
-        try {
-          const val = await cache.store.mget(...keys);
-          val.forEach((v, i) => {
-            if (values[i] === undefined && v !== undefined) values[i] = v;
-          });
-        } catch (e) {}
-      }
-      return values;
-    },
-    mset: async (args: [string, unknown][], ttl?: Milliseconds) => {
-      await Promise.all(caches.map((cache) => cache.store.mset(args, ttl)));
-    },
-    mdel: async (...keys: string[]) => {
-      await Promise.all(caches.map((cache) => cache.store.mdel(...keys)));
-    },
-  };
+	const set = async <T>(
+		key: string,
+		data: T,
+		ttl?: Milliseconds | undefined,
+	) => {
+		await Promise.all(caches.map(async cache => cache.set(key, data, ttl)));
+	};
+
+	return {
+		get,
+		set,
+		async del(key) {
+			await Promise.all(caches.map(async cache => cache.del(key)));
+		},
+		async wrap<T>(
+			key: string,
+			function_: () => Promise<T>,
+			ttl?: WrapTTL<T>,
+			refreshThreshold?: Milliseconds,
+		): Promise<T> {
+			let value: T | undefined;
+			let i = 0;
+			for (; i < caches.length; i++) {
+				try {
+					value = await caches[i].get<T>(key);
+					if (value !== undefined) {
+						break;
+					}
+				} catch {}
+			}
+
+			if (value === undefined) {
+				const result = await function_();
+				const cacheTTL = typeof ttl === 'function' ? ttl(result) : ttl;
+				await set<T>(key, result, cacheTTL);
+				return result;
+			}
+
+			const cacheTTL = typeof ttl === 'function' ? ttl(value) : ttl;
+			Promise.all(
+				caches.slice(0, i).map(async cache => cache.set(key, value, cacheTTL)),
+			).then();
+			caches[i].wrap(key, function_, ttl, refreshThreshold).then(); // Call wrap for store for internal refreshThreshold logic, see: src/caching.ts caching.wrap
+
+			return value;
+		},
+		async reset() {
+			await Promise.all(caches.map(async x => x.reset()));
+		},
+		async mget(...keys: string[]) {
+			const values = Array.from({length: keys.length}).fill(undefined);
+			for (const cache of caches) {
+				if (values.every(x => x !== undefined)) {
+					break;
+				}
+
+				try {
+					const value = await cache.store.mget(...keys);
+					for (const [i, v] of value.entries()) {
+						if (values[i] === undefined && v !== undefined) {
+							values[i] = v;
+						}
+					}
+				} catch {}
+			}
+
+			return values;
+		},
+		async mset(arguments_: Array<[string, unknown]>, ttl?: Milliseconds) {
+			await Promise.all(caches.map(async cache => cache.store.mset(arguments_, ttl)));
+		},
+		async mdel(...keys: string[]) {
+			await Promise.all(caches.map(async cache => cache.store.mdel(...keys)));
+		},
+	};
 }
--- src/stores/memory.ts
+++ src/stores/memory.ts
@@ -1,99 +1,111 @@
-import { LRUCache } from 'lru-cache';
+import {LRUCache} from 'lru-cache';
 import cloneDeep from 'lodash.clonedeep';
-
-import { Config, Cache, Store } from '../caching';
+import {type Config, type Cache, type Store} from '../caching';
 
 function clone<T>(object: T): T {
-  if (typeof object === 'object' && object !== null) {
-    return cloneDeep(object);
-  }
-  return object;
+	if (typeof object === 'object' && object !== null) {
+		return cloneDeep(object);
+	}
+
+	return object;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type LRU = LRUCache<string, any, unknown>;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type LRU = LRUCache<string, any>;
+
 type Pre = LRUCache.OptionsTTLLimit<string, any, unknown>;
 type Options = Omit<Pre, 'ttlAutopurge'> & Partial<Pick<Pre, 'ttlAutopurge'>>;
 export type MemoryConfig = {
-  max?: number;
-  sizeCalculation?: (value: unknown, key: string) => number;
-  shouldCloneBeforeSet?: boolean;
+	max?: number;
+	sizeCalculation?: (value: unknown, key: string) => number;
+	shouldCloneBeforeSet?: boolean;
 } & Options &
-  Config;
+Config;
 
 export type MemoryStore = Store & {
-  get size(): number;
-  dump: LRU['dump'];
-  load: LRU['load'];
-  calculatedSize: LRU['calculatedSize'];
+	get size(): number;
+	dump: LRU['dump'];
+	load: LRU['load'];
+	calculatedSize: LRU['calculatedSize'];
 };
 export type MemoryCache = Cache<MemoryStore>;
 
 /**
  * Wrapper for lru-cache.
  */
-export function memoryStore(args?: MemoryConfig): MemoryStore {
-  const shouldCloneBeforeSet = args?.shouldCloneBeforeSet !== false; // clone by default
-  const isCacheable = args?.isCacheable ?? ((val) => val !== undefined);
+export function memoryStore(arguments_?: MemoryConfig): MemoryStore {
+	const shouldCloneBeforeSet = arguments_?.shouldCloneBeforeSet !== false; // Clone by default
+	const isCacheable = arguments_?.isCacheable ?? (value => value !== undefined);
 
-  const lruOpts = {
-    ttlAutopurge: true,
-    ...args,
-    max: args?.max || 500,
-    ttl: args?.ttl !== undefined ? args.ttl : 0,
-  };
+	const lruOptions = {
+		ttlAutopurge: true,
+		...arguments_,
+		max: arguments_?.max || 500,
+		ttl: arguments_?.ttl === undefined ? 0 : arguments_.ttl,
+	};
 
-  const lruCache = new LRUCache(lruOpts);
+	const lruCache = new LRUCache(lruOptions);
 
-  return {
-    async del(key) {
-      lruCache.delete(key);
-    },
-    get: async <T>(key: string) => lruCache.get(key) as T,
-    keys: async () => [...lruCache.keys()],
-    mget: async (...args) => args.map((x) => lruCache.get(x)),
-    async mset(args, ttl?) {
-      const opt = { ttl: ttl !== undefined ? ttl : lruOpts.ttl } as const;
-      for (const [key, value] of args) {
-        if (!isCacheable(value))
-          throw new Error(`no cacheable value ${JSON.stringify(value)}`);
-        if (shouldCloneBeforeSet) lruCache.set(key, clone(value), opt);
-        else lruCache.set(key, value, opt);
-      }
-    },
-    async mdel(...args) {
-      for (const key of args) lruCache.delete(key);
-    },
-    async reset() {
-      lruCache.clear();
-    },
-    ttl: async (key) => lruCache.getRemainingTTL(key),
-    async set(key, value, opt) {
-      if (!isCacheable(value))
-        throw new Error(`no cacheable value ${JSON.stringify(value)}`);
-      if (shouldCloneBeforeSet) value = clone(value);
+	return {
+		async del(key) {
+			lruCache.delete(key);
+		},
+		get: async <T>(key: string) => lruCache.get(key) as T,
+		keys: async () => [...lruCache.keys()],
+		mget: async (...arguments_) => arguments_.map(x => lruCache.get(x)),
+		async mset(arguments_, ttl?) {
+			const opt = {ttl: ttl === undefined ? lruOptions.ttl : ttl} as const;
+			for (const [key, value] of arguments_) {
+				if (!isCacheable(value)) {
+					throw new Error(`no cacheable value ${JSON.stringify(value)}`);
+				}
 
-      const ttl = opt !== undefined ? opt : lruOpts.ttl;
+				if (shouldCloneBeforeSet) {
+					lruCache.set(key, clone(value), opt);
+				} else {
+					lruCache.set(key, value, opt);
+				}
+			}
+		},
+		async mdel(...arguments_) {
+			for (const key of arguments_) {
+				lruCache.delete(key);
+			}
+		},
+		async reset() {
+			lruCache.clear();
+		},
+		ttl: async key => lruCache.getRemainingTTL(key),
+		async set(key, value, opt) {
+			if (!isCacheable(value)) {
+				throw new Error(`no cacheable value ${JSON.stringify(value)}`);
+			}
 
-      lruCache.set(key, value, { ttl });
-    },
-    get calculatedSize() {
-      return lruCache.calculatedSize;
-    },
-    /**
+			if (shouldCloneBeforeSet) {
+				value = clone(value);
+			}
+
+			const ttl = opt === undefined ? lruOptions.ttl : opt;
+
+			lruCache.set(key, value, {ttl});
+		},
+		get calculatedSize() {
+			return lruCache.calculatedSize;
+		},
+		/**
      * This method is not available in the caching modules.
      */
-    get size() {
-      return lruCache.size;
-    },
-    /**
+		get size() {
+			return lruCache.size;
+		},
+		/**
      * This method is not available in the caching modules.
      */
-    dump: () => lruCache.dump(),
-    /**
+		dump: () => lruCache.dump(),
+		/**
      * This method is not available in the caching modules.
      */
-    load: (...args: Parameters<LRU['load']>) => lruCache.load(...args),
-  };
+		load(...arguments_: Parameters<LRU['load']>) {
+			lruCache.load(...arguments_);
+		},
+	};
 }
