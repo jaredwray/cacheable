cf890f96662eb59b9d9718452ec1aff5c6c9bb27
Jared Wray <me@jaredwray.com>
2024-10-03 16:09:13 -0700

    adding in cache and standard options

--- packages/file-entry-cache/src/index.ts
+++ packages/file-entry-cache/src/index.ts
@@ -1,9 +1,38 @@
-import {FlatCache} from 'flat-cache';
+import {FlatCache, type FlatCacheOptions} from 'flat-cache';
+
+export type FileEntryCacheOptions = {
+	currentWorkingDirectory?: string;
+	useCheckSum?: boolean;
+	cache?: FlatCacheOptions;
+};
 
 export class FileEntryCache {
-	cache: FlatCache;
+	private _cache: FlatCache = new FlatCache();
+	private _useCheckSum = false;
 
-	constructor() {
-		this.cache = new FlatCache();
+	constructor(options?: FileEntryCacheOptions) {
+		if (options?.cache) {
+			this._cache = new FlatCache(options.cache);
+		}
+
+		if (options?.useCheckSum) {
+			this._useCheckSum = options.useCheckSum;
+		}
+	}
+
+	public get cache(): FlatCache {
+		return this._cache;
+	}
+
+	public set cache(cache: FlatCache) {
+		this._cache = cache;
+	}
+
+	public get useCheckSum(): boolean {
+		return this._useCheckSum;
+	}
+
+	public set useCheckSum(value: boolean) {
+		this._useCheckSum = value;
 	}
 }
--- packages/file-entry-cache/test/index.test.ts
+++ packages/file-entry-cache/test/index.test.ts
@@ -1,13 +1,31 @@
-import fs from 'node:fs';
 import {describe, test, expect} from 'vitest';
 import {FileEntryCache} from '../src/index.js';
 
 // eslint-disable-next-line no-promise-executor-return
 const sleep = async (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-describe('file-entry-cache', () => {
+describe('file-entry-cache with options', () => {
 	test('should initialize', () => {
-		const cache = new FileEntryCache();
-		expect(cache).toBeDefined();
+		const fileEntryCache = new FileEntryCache();
+		expect(fileEntryCache).toBeDefined();
+	});
+
+	test('should be able to set flat-cache options', () => {
+		const fileEntryCache = new FileEntryCache({cache: {cacheId: 'test'}});
+		expect(fileEntryCache.cache.cacheId).toBe('test');
+	});
+
+	test('should be able to get and set FlatCache', () => {
+		const fileEntryCache = new FileEntryCache();
+		const newFileEntryCache = new FileEntryCache();
+		fileEntryCache.cache = newFileEntryCache.cache;
+		expect(fileEntryCache.cache).toBe(newFileEntryCache.cache);
+	});
+
+	test('should be able to get and set useCheckSum', () => {
+		const fileEntryCache = new FileEntryCache({useCheckSum: true});
+		expect(fileEntryCache.useCheckSum).toBe(true);
+		fileEntryCache.useCheckSum = false;
+		expect(fileEntryCache.useCheckSum).toBe(false);
 	});
 });
