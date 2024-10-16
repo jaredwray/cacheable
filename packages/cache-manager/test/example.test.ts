f11f610936c75891e87fc34907d8c59db3eb69c9
Jared Wray <me@jaredwray.com>
2024-10-16 08:47:07 -0700

    adding examples and updating vitest

---
+++ packages/cache-manager/test/example.test.ts
@@ -0,0 +1,35 @@
+import {describe, expect, test} from 'vitest';
+import {Keyv} from 'keyv';
+import KeyvRedis from '@keyv/redis';
+import {CacheableMemory, KeyvCacheableMemory} from 'cacheable';
+import {createCache} from '../src/index.js';
+
+describe('examples of cache-manager', async () => {
+	test('set and get with multiple stores', async () => {
+		// Multiple stores
+		const cache = createCache({
+			stores: [
+				//  High performance in-memory cache with LRU and TTL
+				new Keyv({
+					store: new CacheableMemory({ttl: 60_000, lruSize: 5000}),
+				}),
+
+				//  Redis Store
+				new Keyv({
+					store: new KeyvRedis('redis://localhost:6379'),
+				}),
+			],
+		});
+		await cache.set('foo', 'bar');
+		const value = await cache.get('foo');
+		expect(value).toBe('bar');
+	});
+	test('set and get with KeyvCacheableMemory', async () => {
+		const store = new KeyvCacheableMemory({ttl: 60_000, lruSize: 5000});
+		const keyv = new Keyv({store});
+		const cache = createCache({stores: [keyv]});
+		await cache.set('foo', 'bar');
+		const value = await cache.get('foo');
+		expect(value).toBe('bar');
+	});
+});
