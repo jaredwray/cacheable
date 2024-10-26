e13113add8bd8a057671668e586b4382289b60f4
Jared Wray <me@jaredwray.com>
2024-10-26 13:43:48 -0700

    Cacheable - adding namespace support for KeyvCacheableMemory storage adapter

--- packages/cacheable/src/keyv-memory.ts
+++ packages/cacheable/src/keyv-memory.ts
@@ -1,7 +1,15 @@
 import {type KeyvStoreAdapter, type StoredData} from 'keyv';
 import {CacheableMemory, type CacheableMemoryOptions} from './memory.js';
 
+export type KeyvCacheableMemoryOptions = CacheableMemoryOptions & {
+	namespace?: string;
+};
+
 export class KeyvCacheableMemory implements KeyvStoreAdapter {
+
+	private _defaultCache = new CacheableMemory();
+	private _nCache = new Map<string, CacheableMemory>();
+
 	opts: CacheableMemoryOptions = {
 		ttl: 0,
 		useClone: true,
@@ -9,17 +17,26 @@ export class KeyvCacheableMemory implements KeyvStoreAdapter {
 		checkInterval: 0,
 	};
 
-	namespace?: string | undefined;
-	private readonly _cache = new CacheableMemory();
-	constructor(options?: CacheableMemoryOptions) {
+	namespace?: string;
+
+	constructor(options?: KeyvCacheableMemoryOptions) {
 		if (options) {
 			this.opts = options;
-			this._cache = new CacheableMemory(options);
+			this._defaultCache = new CacheableMemory(options);
+
+			if(options.namespace) {
+				this.namespace = options.namespace;
+				this._nCache.set(this.namespace, new CacheableMemory(options));
+			}
 		}
 	}
 
+	public get store() : CacheableMemory {
+		return this.getStore(this.namespace);
+	}
+
 	async get<Value>(key: string): Promise<StoredData<Value> | undefined> {
-		const result = this._cache.get<Value>(key);
+		const result = this.getStore(this.namespace).get<Value>(key);
 		if (result) {
 			return result;
 		}
@@ -28,38 +45,50 @@ export class KeyvCacheableMemory implements KeyvStoreAdapter {
 	}
 
 	async getMany<Value>(keys: string[]): Promise<Array<StoredData<Value | undefined>>> {
-		const result = this._cache.getMany<Value>(keys);
+		const result = this.getStore(this.namespace).getMany<Value>(keys);
 
 		return result;
 	}
 
 	async set(key: string, value: any, ttl?: number): Promise<void> {
-		this._cache.set(key, value, ttl);
+		this.getStore(this.namespace).set(key, value, ttl);
 	}
 
 	async setMany(values: Array<{key: string; value: any; ttl?: number}>): Promise<void> {
-		this._cache.setMany(values);
+		this.getStore(this.namespace).setMany(values);
 	}
 
 	async delete(key: string): Promise<boolean> {
-		this._cache.delete(key);
+		this.getStore(this.namespace).delete(key);
 		return true;
 	}
 
 	async deleteMany?(key: string[]): Promise<boolean> {
-		this._cache.deleteMany(key);
+		this.getStore(this.namespace).deleteMany(key);
 		return true;
 	}
 
 	async clear(): Promise<void> {
-		this._cache.clear();
+		this.getStore(this.namespace).clear();
 	}
 
 	async has?(key: string): Promise<boolean> {
-		return this._cache.has(key);
+		return this.getStore(this.namespace).has(key);
 	}
 
 	on(event: string, listener: (...arguments_: any[]) => void): this {
 		return this;
+	}
+
+	public getStore(namespace?: string): CacheableMemory {
+		if(!namespace) {
+			return this._defaultCache;
+		}
+		
+		if (!this._nCache.has(namespace)) {
+			this._nCache.set(namespace, new CacheableMemory(this.opts));
+		}
+
+		return this._nCache.get(namespace) as CacheableMemory;
 	}
 }
