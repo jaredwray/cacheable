de2b78fc01ac9f94f4f12e6524dcc47f0dd13b80
Jared Wray <me@jaredwray.com>
2024-12-11 16:57:13 -0800

    cacheable - upgrading vitest to 2.1.8 (#940)

--- packages/cacheable/src/coalesce-async.ts
+++ packages/cacheable/src/coalesce-async.ts
@@ -24,6 +24,7 @@ function addCallbackToKey<T>(key: string, callback: PromiseCallback<T>): void {
 }
 
 function getCallbacksByKey<T>(key: string): Array<PromiseCallback<T>> {
+	/* c8 ignore next 1 */
 	return callbacks.get(key) ?? [];
 }
 
@@ -44,6 +45,7 @@ function coalesce<T>(options: {key: string; error?: Error; result?: T}): void {
 	const {key, error, result} = options;
 
 	for (const callback of dequeue(key)) {
+		/* c8 ignore next 1 */
 		if (error) {
 			/* c8 ignore next 3 */
 			callback.reject(error);
@@ -80,6 +82,7 @@ export async function coalesceAsync<T>(
 			const result = await Promise.resolve(fnc());
 			coalesce({key, result});
 			return result;
+		/* c8 ignore next 1 */
 		} catch (error: any) {
 			/* c8 ignore next 5 */
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
