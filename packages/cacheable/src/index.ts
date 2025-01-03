2ee0495ae9a604480713253063d4f74672a4025b
Jared Wray <me@jaredwray.com>
2025-01-03 12:06:12 -0800

    cacheable - feat: adding in jsDoc on CacheableOptions (#968)

--- packages/cacheable/src/index.ts
+++ packages/cacheable/src/index.ts
@@ -23,11 +23,32 @@ export enum CacheableEvents {
 }
 
 export type CacheableOptions = {
+	/**
+	 * The primary store for the cacheable instance
+	 */
 	primary?: Keyv | KeyvStoreAdapter;
+	/**
+	 * The secondary store for the cacheable instance
+	 */
 	secondary?: Keyv | KeyvStoreAdapter;
+	/**
+	 * Whether to enable statistics for the cacheable instance
+	 */
 	stats?: boolean;
+	/**
+	 * Whether the secondary store is non-blocking mode. It is set to false by default.
+	 * If it is set to true then the secondary store will not block the primary store.
+	 */
 	nonBlocking?: boolean;
+	/**
+	 * The time-to-live for the cacheable instance and will be used as the default value.
+	 * can be a number in milliseconds or a human-readable format such as `1s` for 1 second or `1h` for 1 hour
+	 * or undefined if there is no time-to-live.
+	 */
 	ttl?: number | string;
+	/**
+	 * The namespace for the cacheable instance. It can be a string or a function that returns a string.
+	 */
 	namespace?: string | (() => string);
 };
 
