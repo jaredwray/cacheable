23bbfb0e65d33c8d7f0833a9c86c1f4e7238ecce
Jared Wray <me@jaredwray.com>
2025-06-07 13:05:22 -0700

    cacheable-request - chore: upgrading xo to 1.0.5

--- packages/cacheable-request/test/dependencies.test.ts
+++ packages/cacheable-request/test/dependencies.test.ts
@@ -5,9 +5,7 @@ test('@types/http-cache-semantics is a regular (not dev) dependency', () => {
 	// Required to avoid `Could not find a declaration file for module 'http-cache-semantics'` error from `tsc` when using this package in other projects
 
 	// Arrange
-	const packageJsonContents = JSON.parse(
-		readFileSync(new URL('../package.json', import.meta.url), 'utf8'),
-	);
+	const packageJsonContents = JSON.parse(readFileSync(new URL('../package.json', import.meta.url), 'utf8'));
 
 	// Assert
 	expect(packageJsonContents).toHaveProperty('dependencies.@types/http-cache-semantics');
