b85eb6a3ca384c6d29881e43574ffdc15990df80
Jared Wray <me@jaredwray.com>
2025-07-30 11:46:20 -0700

    fixing with emit mocks

--- packages/memoize/test/mock-cacheable.ts
+++ packages/memoize/test/mock-cacheable.ts
@@ -1,55 +1,97 @@
+import {type CacheableStoreItem} from '@cacheable/utils';
 import {type CacheInstance, type CacheSyncInstance} from '../src/index.js';
 
 export class MockCacheable implements CacheInstance<any> {
-	private cache: Record<string, any> = {};
+	private readonly cache = new Map<string, CacheableStoreItem>();
+	private readonly listeners: Record<string, Array<(...args: any[]) => void>> = {};
 
 	async get(key: string): Promise<any | undefined> {
-		return this.cache[key];
+		const item = this.cache.get(key);
+		if (item?.expires && Date.now() > item.expires) {
+			this.cache.delete(key);
+			return undefined;
+		}
+
+		return item?.value;
 	}
 
 	async has(key: string): Promise<boolean> {
-		return key in this.cache;
+		return await this.get(key) !== undefined;
 	}
 
 	async set(key: string, value: any, ttl?: number | string): Promise<boolean> {
+		let expires: number | undefined;
+		if (ttl && typeof ttl === 'number' && ttl > 0) {
+			expires = Date.now() + ttl;
+		}
+
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-		this.cache[key] = value;
+		this.cache.set(key, {key, value, expires});
 		return true;
 	}
 
 	on(event: string, listener: (...args: any[]) => void): void {
-		// Mock implementation
+		this.listeners[event] ||= [];
+
+		this.listeners[event].push(listener);
 	}
 
 	emit(event: string, ...args: any[]): boolean {
-		// Mock implementation
+		const listeners = this.listeners[event];
+		if (listeners) {
+			for (const listener of listeners) {
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+				listener(...args);
+			}
+		}
+
 		return true;
 	}
 }
 
 export class MockCacheableMemory implements CacheSyncInstance<any> {
-	private cache: Record<string, any> = {};
+	private readonly cache = new Map<string, CacheableStoreItem>();
+	private readonly listeners: Record<string, Array<(...args: any[]) => void>> = {};
 
 	get(key: string): any | undefined {
-		return this.cache[key];
+		const result = this.cache.get(key);
+		if (result?.expires && Date.now() > result.expires) {
+			this.cache.delete(key);
+			return undefined;
+		}
+
+		return result?.value;
 	}
 
 	has(key: string): boolean {
-		return key in this.cache;
+		return this.get(key) !== undefined;
 	}
 
 	set(key: string, value: any, ttl?: number | string): boolean {
+		let expires: number | undefined;
+		if (ttl && typeof ttl === 'number' && ttl > 0) {
+			expires = Date.now() + ttl;
+		}
+
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-		this.cache[key] = value;
+		this.cache.set(key, {key, value, expires});
 		return true;
 	}
 
 	on(event: string, listener: (...args: any[]) => void): void {
-		// Mock implementation
+		this.listeners[event] ||= [];
+		this.listeners[event].push(listener);
 	}
 
 	emit(event: string, ...args: any[]): boolean {
-		// Mock implementation
+		const listeners = this.listeners[event];
+		if (listeners) {
+			for (const listener of listeners) {
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+				listener(...args);
+			}
+		}
+
 		return true;
 	}
 }
