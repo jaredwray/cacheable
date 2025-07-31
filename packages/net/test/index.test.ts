e41c5e3bd1b762a5c984219c7877d1144ffc60e1
Jared Wray <me@jaredwray.com>
2025-07-31 12:50:49 -0700

    Update index.test.ts

--- packages/net/test/index.test.ts
+++ packages/net/test/index.test.ts
@@ -15,13 +15,19 @@ describe('Cacheable Net', () => {
 		expect(net).toBeInstanceOf(CacheableNet);
 	});
 
-	test('should create an instance with cache instance', () => {
+	test('should create an instance with cache instance', async () => {
 		const cacheOptions: CacheableNetOptions = {
 			cache: new Cacheable({ttl: '1h'}),
 		};
 		const net = new CacheableNet(cacheOptions);
 		expect(net.cache).toBeInstanceOf(Cacheable);
 		expect(net.cache.ttl).toBe('1h');
+
+		// Do a quick test to ensure the cache is working
+		const data = {key: faker.string.uuid(), value: faker.string.alpha(10)};
+		await net.cache.set(data.key, data.value);
+		const cachedValue = await net.cache.get(data.key);
+		expect(cachedValue).toBe(data.value);
 	});
 
 	test('should create an instance with custom cache options', () => {
