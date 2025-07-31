4760bd7eca47a51ec6a81cb8ab435b4ccc5bf254
Jared Wray <me@jaredwray.com>
2025-07-31 12:42:35 -0700

    moving to net naming

--- packages/net/test/index.test.ts
+++ packages/net/test/index.test.ts
@@ -6,22 +6,22 @@ import {CacheableNet, Net, type CacheableNetOptions} from '../src/index.js';
 
 describe('Cacheable Net', () => {
 	test('should create an instance of CacheableNet', () => {
-		const instance = new CacheableNet();
-		expect(instance).toBeInstanceOf(CacheableNet);
+		const net = new CacheableNet();
+		expect(net).toBeInstanceOf(CacheableNet);
 	});
 
 	test('should create an instance of Net', () => {
-		const instance = new Net();
-		expect(instance).toBeInstanceOf(CacheableNet);
+		const net = new Net();
+		expect(net).toBeInstanceOf(CacheableNet);
 	});
 
 	test('should create an instance with cache instance', () => {
 		const cacheOptions: CacheableNetOptions = {
 			cache: new Cacheable({ttl: '1h'}),
 		};
-		const instance = new CacheableNet(cacheOptions);
-		expect(instance.cache).toBeInstanceOf(Cacheable);
-		expect(instance.cache.ttl).toBe('1h');
+		const net = new CacheableNet(cacheOptions);
+		expect(net.cache).toBeInstanceOf(Cacheable);
+		expect(net.cache.ttl).toBe('1h');
 	});
 
 	test('should create an instance with custom cache options', () => {
@@ -30,13 +30,13 @@ describe('Cacheable Net', () => {
 				ttl: '2h',
 			},
 		};
-		const instance = new CacheableNet(cacheOptions);
-		expect(instance.cache).toBeInstanceOf(Cacheable);
-		expect(instance.cache.ttl).toBe('2h');
+		const net = new Net(cacheOptions);
+		expect(net.cache).toBeInstanceOf(Cacheable);
+		expect(net.cache.ttl).toBe('2h');
 
 		// Set a new cache instance
 		const newCache = new Cacheable({ttl: '3h'});
-		instance.cache = newCache;
-		expect(instance.cache).toBe(newCache);
+		net.cache = newCache;
+		expect(net.cache).toBe(newCache);
 	});
 });
