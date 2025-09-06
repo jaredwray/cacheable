8d2d5cbe2dc6facd5dc88724f8a0d1f0e5738f5a
Jared Wray <me@jaredwray.com>
2025-09-06 14:10:11 -0700

    cache-manager - feat: using @cacheable/utils for helper functions (#1312)

--- packages/cache-manager/src/is-object.ts
+++
@@ -1,5 +0,0 @@
-export function isObject<T = Record<string, unknown>>(
-	value: unknown,
-): value is T {
-	return value !== null && typeof value === "object" && !Array.isArray(value);
-}
--- packages/cache-manager/src/lt.ts
+++
@@ -1,5 +0,0 @@
-export function lt(number1?: number, number2?: number) {
-	return typeof number1 === "number" && typeof number2 === "number"
-		? number1 < number2
-		: false;
-}
